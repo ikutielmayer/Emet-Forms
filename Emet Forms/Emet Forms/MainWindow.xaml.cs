532c6d32f926f7363d86c2430224b548a6d00fe5
gerardo mayer <gerardomayer@hotmail.com>
2020-05-07 22:09:59 +0300

    coneccion a la base de datos

--- Emet Forms/Emet Forms/MainWindow.xaml.cs
+++ Emet Forms/Emet Forms/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,17 +13,37 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using MySql.Data;
+using MySql.Data.MySqlClient;
 
 namespace Emet_Forms
 {
-    /// <summary>
-    /// Lógica de interacción para MainWindow.xaml
-    /// </summary>
+ 
     public partial class MainWindow : Window
     {
+        
         public MainWindow()
         {
             InitializeComponent();
+
+          //  string connectionstring = "SERVER=localhost;DATABASE=emet;UID=ikutiel;PASSWORD=B7654321b;";
+            string con = "server = localhost; user id = ikutiel; password = B7654321b; persistsecurityinfo = True; database = emet";
+            MySqlConnection connection = new MySqlConnection(con);
+
+            MySqlCommand cmd = new MySqlCommand("select * from reportes", connection);
+
+            connection.Open();
+            DataTable dt = new DataTable();
+            dt.Load(cmd.ExecuteReader());
+            connection.Close();
+            
+            dtGrid.DataContext = dt;
+
+        }
+
+        private void btnEnviar_Click(object sender, RoutedEventArgs e)
+        {
+           
         }
     }
 }
